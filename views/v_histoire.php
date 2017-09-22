bf123dbea829a6994294bb0adecab3c538a23a81
KuraGon <bilel.b136@gmail.com>
2017-09-22 17:20:53 +0200

    mvc 0.0.2
    bilel

--- views/v_histoire.php
+++ views/v_histoire.php
@@ -1,53 +1,12 @@
 
-     <header class="masthead-h">
-      <div class="header-content">
-        <div class="header-content-inner">
-          <h1 class="shadow-t" id="homeHeading">Marché de la criée.</h1>
-          <hr class="blue">
-          <a class="btn btn-primary btn-xl js-scroll-trigger" href="#about">Voir plus!</a>
-        </div>
-        <br> <br> <br>
-        <img class="img100" src="./asset/images/scroll-down.gif">
-      </div>
+     <header class="masthead-a">
     </header>
 
 <section class="bg-primary id="services">
       <div class="container">
-        <div class="row">
-          <div class="col-lg-12 text-center">
-            <h1 class="section-heading">Un titre de ouf:</h2>
-                <div class="container">
-    <div class="row">
-            <hr class="light">
-            <br>
-          </div>
-        </div>
-        <div class="col-sm-2"><br><br><br>
-            <nav class="nav-sidebar">
-                <ul class="nav">
-                    <li class="active"><a href="javascript:;">Home</a></li>
-                    <li><a href="javascript:;">MACHIN FUN</a></li>
-                    <li><a href="javascript:;">TRUCS DE OUF</a></li>
-                    <li><a href="javascript:;">JSP</a></li>
-                    <li class="nav-divider"></li>
-                </ul>
-            </nav>
-        </div>
-           <div class="col-md-10 text-center">
-
-            <h3>Exactement :</h3><br>
-             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris rhoncus augue eu lacinia efficitur. Maecenas nec malesuada justo. Vivamus scelerisque libero at accumsan suscipit. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean cursus velit vitae nibh rhoncus laoreet. In hac habitasse platea dictumst. Cras ullamcorper eros ac libero ultricies, sed condimentum massa convallis. Sed vitae convallis libero. Maecenas gravida libero nibh. Nullam ac porta sem.
-
-Integer aliquet ex a nibh mattis sollicitudin. Cras vulputate nec elit eget consectetur. Phasellus auctor mauris a nunc sollicitudin, non tempus erat aliquet. Proin auctor ante vel dolor euismod bibendum. Nunc luctus nec lectus nec cursus. Aliquam volutpat massa varius venenatis maximus. Curabitur consectetur suscipit consequat. Donec sed lacinia justo, vitae bibendum augue. Sed faucibus ipsum ipsum, et lacinia arcu consectetur in. Etiam quis ex at lectus pharetra varius. Ut risus erat, sagittis id tempor et, accumsan ut diam. Donec pellentesque erat in vulputate tempus. Phasellus commodo diam a finibus egestas. In hac habitasse platea dictumst. Quisque mi nulla, ornare vehicula dictum a, maximus ut sapien.
-
-Sed id ultricies enim. Cras dapibus ultricies convallis. Maecenas egestas nisl facilisis eros lobortis, eu dapibus lorem aliquet. Nullam mattis urna eu elementum bibendum. Suspendisse vitae ipsum non tellus pellentesque hendrerit. Maecenas feugiat venenatis tortor, at lobortis elit gravida eu. Sed placerat mattis nunc, non dictum mauris finibus vitae. Aliquam pulvinar pulvinar enim ac sollicitudin. In imperdiet accumsan ultrices. Suspendisse pellentesque, tortor ac ornare pulvinar, arcu orci lobortis turpis, non placerat mi velit eget diam.
-
-Quisque faucibus, tortor sit amet tincidunt elementum, lacus risus suscipit neque, in mattis enim velit vitae ante. Fusce gravida nisl in ante mollis mattis. Morbi ultrices ipsum eget sapien auctor elementum. Aenean ut risus at justo vestibulum pharetra. Vivamus placerat et tortor ut interdum. Duis eu tortor libero. Curabitur blandit tincidunt tortor ut auctor.
-
-Sed ut velit quam. Nullam tincidunt viverra tristique. Pellentesque pulvinar mauris sit amet lacus mattis pharetra vel quis eros. Duis semper magna et lorem interdum, a finibus lectus pretium. Etiam dignissim enim in aliquam malesuada. Proin scelerisque nisl mauris, id consequat mi aliquet id. Sed faucibus justo ac justo vehicula feugiat. Donec ultricies quam ac odio accumsan dictum. Sed sit amet nulla varius, malesuada ex eu, egestas mauris. Vestibulum metus magna, accumsan quis sem et, venenatis scelerisque urna.
-
-</p>
-              </div>
+        <div class="row text-center">
+       
+      </div>
       </div>
     </section>
      <section id="about">
