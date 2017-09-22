f5711ccc586c8998600cfd5bb99e36c29c519227
KuraGon <bilel.b136@gmail.com>
2017-09-22 17:25:12 +0200

    mvc 0.0.4
    once again

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,1 +1,11 @@
-</body></html>
+ <section id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 mx-auto text-center">
+            <h2>YO JE SUIS LE FOOTER</h2>
+          </div>
+        </div>
+      </div>
+    </section>
+</body>
+</html>
--- views/v_activite.php
+++ views/v_activite.php
@@ -26,12 +26,4 @@
       </div>
       </div>
     </section>
-     <section id="about">
-      <div class="container">
-        <div class="row">
-          <div class="col-lg-12 mx-auto text-center">
-            <h2>YO JE SUIS LE FOOTER</h2>
-          </div>
-        </div>
-      </div>
-    </section>
+    
