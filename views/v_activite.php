8131246e0810139d2e4552810459a8402c765af6
KuraGon <bilel.b136@gmail.com>
2017-09-22 17:49:52 +0200

    mvc 0.0.7

--- views/v_activite.php
+++ views/v_activite.php
@@ -2,7 +2,7 @@
      <header class="masthead-a">
     </header>
 
-<section class="bg-primary id="services">
+<section class="bg-primary" id="services">
       <div class="container">
         <div class="row text-center">
         <div class="col-lg-4">
