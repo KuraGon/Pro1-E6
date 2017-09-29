aa74337eae41da7ad653fd5d207996c5250297b4
david <oliverdavid.oliverdavid@gmail.com>
2017-09-29 16:16:01 +0200

    immage article

---
+++ views/v_criee.php
@@ -0,0 +1,12 @@
+<header class="masthead-a">
+    </header>
+
+<section class="bg-primary" id="services">
+  <div class="container">
+    <div class="row text-center">
+      <div class="col-lg-12">
+        <?= $article_criee; ?>     
+      </div>
+    </div>
+  </div>
+</section>
