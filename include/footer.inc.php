14fc36c6e126b3ea1c7d043ac9cf2bccc0351c65
KuraGon <bilel.b136@gmail.com>
2017-09-29 17:05:39 +0200

    ok

--- include/footer.inc.php
+++ include/footer.inc.php
@@ -1,5 +1,8 @@
  <section id="footer" class="color-footer">
-      <div class="container ">
+     <h1>Localisation du quartier:</h1>
+     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5807.89085049136!2d5.360137327367751!3d43.29446456654086!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12c9c0c6a1843729%3A0x7d3f3acf189dc3b1!2sVieux-Port+de+Marseille!5e0!3m2!1sfr!2sfr!4v1506694734212" width="100%" height="280" frameborder="0" style="border:0" allowfullscreen></iframe>
+
+     <div class="container ">
         <span>© 2017, Genesix, All rights reserved.</span>
       </div>
     </section>
